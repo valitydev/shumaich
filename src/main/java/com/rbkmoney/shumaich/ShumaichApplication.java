1da0988a1807480d96799a32b898af7cec82c240
NPospolita <nikita7asics@gmail.com>
2020-04-14 11:08:32 +0300

    lilfix

--- src/main/java/com/rbkmoney/shumaich/ShumaichApplication.java
+++ src/main/java/com/rbkmoney/shumaich/ShumaichApplication.java
@@ -3,12 +3,10 @@ package com.rbkmoney.shumaich;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @ServletComponentScan
 @SpringBootApplication
-@EnableScheduling
 public class ShumaichApplication extends SpringApplication {
 
     public static void main(String[] args) {
