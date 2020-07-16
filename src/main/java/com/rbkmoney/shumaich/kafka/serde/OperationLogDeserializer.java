37143077f0cfdee595a809c24930055b11ee90b1
a.romanov <a.romanov@rbkmoney.com>
2020-07-16 17:36:31 +0300

    BJ-960: Fix code review comments

--- src/main/java/com/rbkmoney/shumaich/kafka/serde/OperationLogDeserializer.java
+++ src/main/java/com/rbkmoney/shumaich/kafka/serde/OperationLogDeserializer.java
@@ -1,38 +1,14 @@
 package com.rbkmoney.shumaich.kafka.serde;
 
 import com.rbkmoney.damsel.shumaich.OperationLog;
+import com.rbkmoney.kafka.common.serialization.AbstractThriftDeserializer;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.common.serialization.Deserializer;
-import org.apache.thrift.TDeserializer;
-import org.apache.thrift.protocol.TBinaryProtocol;
-
-import java.util.Map;
 
 @Slf4j
-public class OperationLogDeserializer implements Deserializer<OperationLog> {
-
-    private final ThreadLocal<TDeserializer> tDeserializerThreadLocal = ThreadLocal.withInitial(
-            () -> new TDeserializer(
-                    new TBinaryProtocol.Factory()));
+public class OperationLogDeserializer extends AbstractThriftDeserializer<OperationLog> {
 
     @Override
     public OperationLog deserialize(String topic, byte[] data) {
-        OperationLog operationLog = new OperationLog();
-
-        try {
-            tDeserializerThreadLocal.get().deserialize(operationLog, data);
-        } catch (Exception e) {
-            log.error("Deserialization error. OperationLog data: {} ", data, e);
-        }
-
-        return operationLog;
-    }
-
-    @Override
-    public void configure(Map<String, ?> configs, boolean isKey) {
-    }
-
-    @Override
-    public void close() {
+        return deserialize(data, new OperationLog());
     }
 }
